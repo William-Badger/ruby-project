4e3cd11e6fbbbb2aed9be83621fa7acad64efff6
William Badger <wkbadger@gmail.com>
2021-08-01 18:05:52 -0500

    v1.00

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import MainContainer from "./MainContainer"
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <MainContainer />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/MainContainer.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const MainContainer = () => {
+    return (
+        <div>
+    <div class="ui secondary pointing menu">
+    <a class="active item">
+    Login
+  </a>
+  <a class="item">
+    Leaderboards
+  </a>
+  <a class="item">
+    Game
+  </a>
+  <div class="right menu">
+    <a class="ui item">
+      Logout
+    </a>
+  </div>
+</div>
+        </div>
+    );
+}
+
+export default MainContainer;
