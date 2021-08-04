4eece6838cc3b41163b3238939256d5726fc3ffc
William Badger <wkbadger@gmail.com>
2021-08-04 17:19:19 -0500

    fetch fix

--- src/GameChoices.js
+++ src/GameChoices.js
@@ -8,7 +8,7 @@ const GameChoices = ({setAtHome, currentUser}) => {
   const [difficulty, setDifficulty] = useState('');
   const [riddles, setRiddles] = useState([]);
 
-  if (difficulty !== '') {
+  function handleClick(difficulty) {
         fetch(`http://localhost:9292/games/${difficulty}`)
           .then((r) => r.json())
           .then((data) => console.log(data));
@@ -24,17 +24,17 @@ const GameChoices = ({setAtHome, currentUser}) => {
         <div className="game">
             <div id="choices-bar" class="ui black three item inverted menu">
   <a class="item"
-  onClick={() => setDifficulty('easy')}
+  onClick={() => handleClick('easy')}
   >
     Easy
   </a>
   <a class="item"
-  onClick={() => setDifficulty('medium')}
+  onClick={() => handleClick('medium')}
   >
     Medium
   </a>
   <a class="item"
-  onClick={() => setDifficulty('hard')}
+  onClick={() => handleClick('hard')}
   >
     Hard
   </a>
